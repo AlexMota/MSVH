a4d4387310e617ee9d1849d9417f27b800500368
paulohsg <paulohenrique_sg@hotmail.com>
2014-09-22 16:28:51 -0300

    Gráficos e botoes da guia arrumados

--- MSVH-app 4.0/js/script.js
+++ MSVH-app 4.0/js/script.js
@@ -1,18 +1,26 @@
-$(function() {
-	$('#pressao').highcharts({
+$(document).on('pageshow', '#graficos', function(){    
+    var chart;
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'pressao',
+                
+            },
 
-		exporting : {
-			enabled : false
-		},
-		credits : {
-			enabled : false
-		},
-		title : {
-			text : 'Pressão Arterial',
-			x : -20 //center
-		},
+            exporting : {
+				enabled : false
+			},
+			credits : {
+				enabled : false
+			},
 
-		xAxis : {
+
+            title: {
+                text : 'Pressão Arterial',
+				x : -20 //center
+            },
+            
+            xAxis : {
 			title : {
 				text : 'Horas'
 			},
@@ -35,8 +43,7 @@ $(function() {
 			shared : true
 
 		},
-
-		legend : {
+            legend : {
 			labelFormatter : function() {
 				if (this.index == 0) {
 					return 'Pressão Arterial Sistólica';
@@ -46,12 +53,13 @@ $(function() {
 					return 'Pressão Arteria Média'
 				}
 			},
+
 			layout : 'vertical',
 			align : 'center',
 			verticalAlign : 'bottom',
 			borderWidth : 0
 		},
-		series : [{
+            series : [{
 
 			name : 'PAS',
 			data : [110, 150, 130, 140, 150, 120, 130, 160, 170, 190, 210, 190, 180, 190, 200, 190, 180, 170, 150, 160, 180, 160, 150, 140],
@@ -73,24 +81,37 @@ $(function() {
 				valueSuffix : ' mmHg'
 			}
 		}]
-	});
+        });
+    });
+    
 });
 
-$(function() {
-	$('#pressaoVenosaEabominal').highcharts({
 
-		exporting : {
-			enabled : false
-		},
-		credits : {
-			enabled : false
-		},
-		title : {
-			text : 'PVC e PIA',
-			x : -20 //center
-		},
 
-		xAxis : {
+
+$(document).on('pageshow', '#graficos', function(){    
+    var chart;
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'saturacaoOxigenio',
+                
+            },
+
+            exporting : {
+				enabled : false
+			},
+			credits : {
+				enabled : false
+			},
+
+            
+            title: {
+                text : 'Sat. O2',
+				x : -20 //center
+            },
+            
+            xAxis : {
 			title : {
 				text : 'Horas'
 			},
@@ -107,55 +128,62 @@ $(function() {
 			}]
 		},
 		tooltip : {
+
 			pointFormat : '{series.name}: <b>{point.y}</b><br/>',
 
 			shared : true
 
 		},
-		legend : {
+            legend : {
 			labelFormatter : function() {
 				if (this.index == 0) {
-					return 'Pressão Venosa Central';
-				} else if (this.index == 1) {
-					return 'Pressão Intra-Abdominal';
-				}
+					return 'Saturação de Oxigênio';
+				} 
 			},
+
 			layout : 'vertical',
 			align : 'center',
 			verticalAlign : 'bottom',
 			borderWidth : 0
 		},
-		series : [{
-			name : 'PVC',
+            series : [{
+			name : 'Sat. O2',
 			data : [5, 8, 7, 4, 3, 2, 3, 5, 6, 8, 8, 8, 8, 7, 5, 2, 3, 4, 5, 3, 4, 6, 5, 7],
 			tooltip : {
-				valueSuffix : ' cmH2O'
-			}
-		}, {
-			name : 'PIA',
-			data : [9, 8, 5, 6, 7, 8, 10, 10, 10, 9, 9, 6, 5, 3, 4, 5, 6, 6, 7, 8, 8, 8, 9, 9],
-			tooltip : {
-				valueSuffix : ' cmH2O'
+				valueSuffix : ' %'
 			}
 		}]
-	});
+        });
+    });
+    
 });
 
-$(function() {
-	$('#frequenciaCardiaca').highcharts({
 
-		exporting : {
-			enabled : false
-		},
-		credits : {
-			enabled : false
-		},
-		title : {
-			text : 'Frequência Cardíaca',
-			x : -20 //center
-		},
 
-		xAxis : {
+
+$(document).on('pageshow', '#graficos', function(){    
+    var chart;
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'frequenciaCardiaca',
+                
+            },
+
+            exporting : {
+				enabled : false
+			},
+			credits : {
+				enabled : false
+			},
+
+            
+            title: {
+                text : 'Frequência Cardíaca',
+				x : -20 //center
+            },
+            
+            xAxis : {
 			title : {
 				text : 'Horas'
 			},
@@ -173,41 +201,57 @@ $(function() {
 		},
 		tooltip : {
 
-			valueSuffix : 'bpm'
+			pointFormat : '{series.name}: <b>{point.y}</b><br/>',
+
+			shared : true
+
 		},
-		legend : {
+            legend : {
 			labelFormatter : function() {
 				if (this.index == 0) {
 					return 'Frequência Cardíaca';
 				}
 			},
+
 			layout : 'vertical',
 			align : 'center',
 			verticalAlign : 'bottom',
 			borderWidth : 0
 		},
-		series : [{
+            series : [{
 			name : 'FC',
 			data : [49, 55, 57, 56, 55, 58, 60, 59, 58, 60, 65, 68, 70, 73, 71, 68, 65, 63, 60, 57, 56, 55, 52, 55]
 		}]
-	});
+        });
+    });
+    
 });
 
-$(function() {
-	$('#temperaturaCorporea').highcharts({
 
-		exporting : {
-			enabled : false
-		},
-		credits : {
-			enabled : false
-		},
-		title : {
-			text : 'Temperatura Corpórea',
-			x : -20 //center
-		},
 
-		xAxis : {
+$(document).on('pageshow', '#graficos', function(){    
+    var chart;
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'temperaturaCorporea',
+                
+            },
+
+            exporting : {
+				enabled : false
+			},
+			credits : {
+				enabled : false
+			},
+
+            
+            title: {
+                text : 'Temperatura Corpórea',
+				x : -20 //center
+            },
+            
+            xAxis : {
 			title : {
 				text : 'Horas'
 			},
@@ -225,41 +269,57 @@ $(function() {
 		},
 		tooltip : {
 
-			valueSuffix : '°C'
+			pointFormat : '{series.name}: <b>{point.y}</b><br/>',
+
+			shared : true
+
 		},
-		legend : {
+            legend : {
 			labelFormatter : function() {
 				if (this.index == 0) {
 					return 'Temperatura Corpórea';
 				}
 			},
+
 			layout : 'vertical',
 			align : 'center',
 			verticalAlign : 'bottom',
 			borderWidth : 0
 		},
-		series : [{
+            series : [{
 			name : 'TC',
 			data : [36.1, 36.1, 36.3, 36.2, 36.1, 36, 35.9, 36, 36.1, 36.5, 36.7, 36.8, 36.8, 36.8, 37, 37.2, 37.3, 37.5, 36.9, 36.5, 36.4, 36.4, 36.5, 36.6]
 		}]
-	});
+        });
+    });
+    
 });
 
-$(function() {
-	$('#frequenciaRespiratoria').highcharts({
 
-		exporting : {
-			enabled : false
-		},
-		credits : {
-			enabled : false
-		},
-		title : {
-			text : 'Frequência Respiratória',
-			x : -20 //center
-		},
 
-		xAxis : {
+$(document).on('pageshow', '#graficos', function(){    
+    var chart;
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'temperaturaCorporea',
+                
+            },
+
+            exporting : {
+				enabled : false
+			},
+			credits : {
+				enabled : false
+			},
+
+            
+            title: {
+                text : 'Temperatura Corpórea',
+				x : -20 //center
+            },
+            
+            xAxis : {
 			title : {
 				text : 'Horas'
 			},
@@ -277,26 +337,105 @@ $(function() {
 		},
 		tooltip : {
 
-			valueSuffix : 'ipm'
+			pointFormat : '{series.name}: <b>{point.y}</b><br/>',
+
+			shared : true
+
 		},
-		legend : {
+            legend : {
 			labelFormatter : function() {
 				if (this.index == 0) {
-					return 'Frequência Respiratória';
+					return 'Temperatura Corpórea';
 				}
 			},
+
 			layout : 'vertical',
 			align : 'center',
 			verticalAlign : 'bottom',
 			borderWidth : 0
 		},
-		series : [{
+            series : [{
+			name : 'TC',
+			data : [36.1, 36.1, 36.3, 36.2, 36.1, 36, 35.9, 36, 36.1, 36.5, 36.7, 36.8, 36.8, 36.8, 37, 37.2, 37.3, 37.5, 36.9, 36.5, 36.4, 36.4, 36.5, 36.6]
+		}]
+        });
+    });
+    
+});
+
+
+$(document).on('pageshow', '#graficos', function(){    
+    var chart;
+    $(document).ready(function() {
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: 'frequenciaRespiratoria',
+                
+            },
+
+            exporting : {
+				enabled : false
+			},
+			credits : {
+				enabled : false
+			},
+
+            
+            title: {
+                text : 'Frequência Respiratória',
+				x : -20 //center
+            },
+            
+            xAxis : {
+			title : {
+				text : 'Horas'
+			},
+			categories : ['07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '00', '01', '02', '03', '04', '05', '06']
+		},
+		yAxis : {
+			title : {
+				text : null
+			},
+			plotLines : [{
+				value : 0,
+				width : 1,
+				color : '#808080'
+			}]
+		},
+		tooltip : {
+
+			pointFormat : '{series.name}: <b>{point.y}</b><br/>',
+
+			shared : true
+
+		},
+            legend : {
+			labelFormatter : function() {
+				if (this.index == 0) {
+					return 'Frequência Respiratória';
+				}
+			},
+
+			layout : 'vertical',
+			align : 'center',
+			verticalAlign : 'bottom',
+			borderWidth : 0
+		},
+            series : [{
 			name : 'FR',
 			data : [15, 13, 12, 13, 13, 14, 14, 13, 12, 15, 16, 15, 13, 17, 18, 12, 13, 15, 17, 18, 15, 16, 13, 12]
 		}]
-	});
+        });
+    });
+    
 });
 
+
+
+
+
+
+/*
 $(function() {
 	$('#glicemia').highcharts({
 
@@ -382,6 +521,8 @@ $(function() {
 
 });
 
+*/
+
 $(function() {
 
 	$('#monFreqCard').click(function() {
@@ -401,11 +542,50 @@ $(function() {
 		document.getElementById('btTempCorp').classList.remove('ui-state-persist');
 		document.getElementById('btPressaoArt').classList.remove('ui-state-persist');
 
-		$('#btFreqCar').click();
+		$('#pressao').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'none');
+		$('#frequenciaCardiaca').css('display', 'flex');
+
+
 
 	});
 
 });
+
+
+$(function() {
+
+	$('#btFreqCar').click(function() {
+
+		var botaoFC = document.getElementById("btFreqCar");
+		botaoFC.classList.add("ui-btn-active");
+		botaoFC.classList.add("ui-state-persist");
+		//console.log(botao);
+
+		document.getElementById('btFreqResp').classList.remove('ui-btn-active');
+		document.getElementById('btSatOxig').classList.remove('ui-btn-active');
+		document.getElementById('btTempCorp').classList.remove('ui-btn-active');
+		document.getElementById('btPressaoArt').classList.remove('ui-btn-active');
+
+		document.getElementById('btFreqResp').classList.remove('ui-state-persist');
+		document.getElementById('btSatOxig').classList.remove('ui-state-persist');
+		document.getElementById('btTempCorp').classList.remove('ui-state-persist');
+		document.getElementById('btPressaoArt').classList.remove('ui-state-persist');
+
+		$('#pressao').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'none');
+		$('#frequenciaCardiaca').css('display', 'flex');
+
+
+
+	});
+
+});
+
 
 $(function() {
 
@@ -426,11 +606,47 @@ $(function() {
 		document.getElementById('btTempCorp').classList.remove('ui-state-persist');
 		document.getElementById('btPressaoArt').classList.remove('ui-state-persist');
 
-		$(botaoFR).click();
+		$('#pressao').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'none');
+		$('#frequenciaCardiaca').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'flex');
 
 	});
 
 });
+
+
+
+$(function() {
+
+	$('#btFreqResp').click(function() {
+
+		var botaoFR = document.getElementById("btFreqResp");
+		botaoFR.classList.add("ui-btn-active");
+		botaoFR.classList.add("ui-state-persist");
+		//console.log(botao);
+
+		document.getElementById('btFreqCar').classList.remove('ui-btn-active');
+		document.getElementById('btSatOxig').classList.remove('ui-btn-active');
+		document.getElementById('btTempCorp').classList.remove('ui-btn-active');
+		document.getElementById('btPressaoArt').classList.remove('ui-btn-active');
+
+		document.getElementById('btFreqCar').classList.remove('ui-state-persist');
+		document.getElementById('btSatOxig').classList.remove('ui-state-persist');
+		document.getElementById('btTempCorp').classList.remove('ui-state-persist');
+		document.getElementById('btPressaoArt').classList.remove('ui-state-persist');
+
+		$('#pressao').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'none');
+		$('#frequenciaCardiaca').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'flex');
+
+	});
+
+});
+
 
 $(function() {
 
@@ -451,11 +667,48 @@ $(function() {
 		document.getElementById('btFreqResp').classList.remove('ui-state-persist');
 		document.getElementById('btPressaoArt').classList.remove('ui-state-persist');
 
-		$(botaoTC).click();
+		$('#pressao').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'none');
+		$('#frequenciaCardiaca').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'flex');
 
 	});
 
 });
+
+
+
+$(function() {
+
+	$('#btTempCorp').click(function() {
+
+		var botaoTC = document.getElementById("btTempCorp");
+		botaoTC.classList.add("ui-btn-active");
+		botaoTC.classList.add("ui-state-persist");
+		//console.log(botao);
+
+		document.getElementById('btFreqCar').classList.remove('ui-btn-active');
+		document.getElementById('btSatOxig').classList.remove('ui-btn-active');
+		document.getElementById('btFreqResp').classList.remove('ui-btn-active');
+		document.getElementById('btPressaoArt').classList.remove('ui-btn-active');
+
+		document.getElementById('btFreqCar').classList.remove('ui-state-persist');
+		document.getElementById('btSatOxig').classList.remove('ui-state-persist');
+		document.getElementById('btFreqResp').classList.remove('ui-state-persist');
+		document.getElementById('btPressaoArt').classList.remove('ui-state-persist');
+
+		$('#pressao').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'none');
+		$('#frequenciaCardiaca').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'flex');
+
+	});
+
+});
+
+
 
 $(function() {
 
@@ -476,11 +729,48 @@ $(function() {
 		document.getElementById('btFreqResp').classList.remove('ui-state-persist');
 		document.getElementById('btPressaoArt').classList.remove('ui-state-persist');
 
-		$(botaoSO).click();
+		$('#pressao').css('display', 'none');
+		$('#frequenciaCardiaca').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'flex');
 
 	});
 
 });
+
+
+
+$(function() {
+
+	$('#btSatOxig').click(function() {
+
+		var botaoSO = document.getElementById("btSatOxig");
+		botaoSO.classList.add("ui-btn-active");
+		botaoSO.classList.add("ui-state-persist");
+		//console.log(botao);
+
+		document.getElementById('btFreqCar').classList.remove('ui-btn-active');
+		document.getElementById('btTempCorp').classList.remove('ui-btn-active');
+		document.getElementById('btFreqResp').classList.remove('ui-btn-active');
+		document.getElementById('btPressaoArt').classList.remove('ui-btn-active');
+
+		document.getElementById('btFreqCar').classList.remove('ui-state-persist');
+		document.getElementById('btTempCorp').classList.remove('ui-state-persist');
+		document.getElementById('btFreqResp').classList.remove('ui-state-persist');
+		document.getElementById('btPressaoArt').classList.remove('ui-state-persist');
+
+		$('#pressao').css('display', 'none');
+		$('#frequenciaCardiaca').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'flex');
+
+	});
+
+});
+
+
 
 $(function() {
 
@@ -501,11 +791,48 @@ $(function() {
 		document.getElementById('btFreqResp').classList.remove('ui-state-persist');
 		document.getElementById('btSatOxig').classList.remove('ui-state-persist');
 
-		$(botaoPA).click();
+		$('#frequenciaCardiaca').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'none');
+		$('#pressao').css('display', 'flex');
 
 	});
 
 });
+
+
+$(function() {
+
+	$('#btPressaoArt').click(function() {
+
+		var botaoPA = document.getElementById("btPressaoArt");
+		botaoPA.classList.add("ui-btn-active");
+		botaoPA.classList.add("ui-state-persist");
+		//console.log(botao);
+
+		document.getElementById('btFreqCar').classList.remove('ui-btn-active');
+		document.getElementById('btTempCorp').classList.remove('ui-btn-active');
+		document.getElementById('btFreqResp').classList.remove('ui-btn-active');
+		document.getElementById('btSatOxig').classList.remove('ui-btn-active');
+
+		document.getElementById('btFreqCar').classList.remove('ui-state-persist');
+		document.getElementById('btTempCorp').classList.remove('ui-state-persist');
+		document.getElementById('btFreqResp').classList.remove('ui-state-persist');
+		document.getElementById('btSatOxig').classList.remove('ui-state-persist');
+
+		$('#frequenciaCardiaca').css('display', 'none');
+		$('#saturacaoOxigenio').css('display', 'none');
+		$('#frequenciaRespiratoria').css('display', 'none');
+		$('#temperaturaCorporea').css('display', 'none');
+		$('#pressao').css('display', 'flex');
+
+	});
+
+});
+
+
+
 
 $(function() {
 	$('#aviso-sonoro').change(function() {
