ae857bdc3b274e94b7678bc03011ea68e66bbbaf
paulohsg <paulohenrique_sg@hotmail.com>
2014-10-29 02:54:20 -0200

    Gráficos gerados de acordo com os dados lidos do servidor

--- MSVH-app 5.0/js/script.js
+++ MSVH-app 5.0/js/script.js
@@ -5,6 +5,23 @@ pacientesJson = [];
 
 $(document).on('pageshow', '#graficos', function() {
 	var chart;
+	var arrayDiasMonitorados = pacientesJson[numIdPacAtual].diasMonitorados;
+	var arrayDadosHora = pacientesJson[numIdPacAtual].diasMonitorados[arrayDiasMonitorados.length-1].dadosHoras;
+	var arrayPAS = [];
+	var arrayPAD = [];
+	var arrayPAM = [];
+
+	for(var i=0; i<arrayDadosHora.length; i++){
+		arrayPAS.push([i, parseFloat(arrayDadosHora[i].pressaoSistolica)]);
+		arrayPAD.push([i, parseFloat(arrayDadosHora[i].pressaoDiastolica)]);
+		arrayPAM.push([i, parseFloat(arrayDadosHora[i].pressaoMedia)]);
+		
+	}
+
+	
+
+
+
 	$(document).ready(function() {
 		chart = new Highcharts.Chart({
 			chart : {
@@ -71,32 +88,49 @@ $(document).on('pageshow', '#graficos', function() {
 			series : [{
 
 				name : 'PAS',
-				data : [110, 150, 130, 140, 150, 120, 130, 160, 170, 190, 210, 190, 180, 190, 200, 190, 180, 170, 150, 160, 180, 160, 150, 140],
+				data : [],
 				tooltip : {
 					valueSuffix : ' mmHg'
 				}
 			}, {
 
 				name : 'PAD',
-				data : [75, 90, 85, 90, 80, 95, 100, 105, 110, 95, 85, 90, 80, 75, 100, 90, 80, 95, 85, 80, 70, 75, 80, 100],
+				data : [],
 				tooltip : {
 					valueSuffix : ' mmHg'
 				}
 			}, {
 
 				name : 'PAM',
-				data : [86, 110, 100, 95, 93, 90, 95, 92, 85, 90, 100, 120, 110, 105, 90, 95, 85, 80, 95, 100, 110, 95, 85, 90],
+				data : [],
 				tooltip : {
 					valueSuffix : ' mmHg'
 				}
 			}]
 		});
+
+				
+				
+
 	});
+
+				chart.series[0].setData(arrayPAS);
+				chart.series[1].setData(arrayPAD);
+				chart.series[2].setData(arrayPAM);
 
 });
 
 $(document).on('pageshow', '#graficos', function() {
 	var chart;
+	var arrayDiasMonitorados = pacientesJson[numIdPacAtual].diasMonitorados;
+	var arrayDadosHora = pacientesJson[numIdPacAtual].diasMonitorados[arrayDiasMonitorados.length-1].dadosHoras;
+	var arraySAT = [];
+
+	for(var i=0; i<arrayDadosHora.length; i++){
+		arraySAT.push([i, parseFloat(arrayDadosHora[i].saturacaoOxigenio)]);
+		
+	}
+
 	$(document).ready(function() {
 		chart = new Highcharts.Chart({
 			chart : {
@@ -160,7 +194,7 @@ $(document).on('pageshow', '#graficos', function() {
 			},
 			series : [{
 				name : 'Sat. O2',
-				data : [5, 8, 7, 4, 3, 2, 3, 5, 6, 8, 8, 8, 8, 7, 5, 2, 3, 4, 5, 3, 4, 6, 5, 7],
+				data : [],
 				tooltip : {
 					valueSuffix : ' %'
 				}
@@ -168,10 +202,22 @@ $(document).on('pageshow', '#graficos', function() {
 		});
 	});
 
+			chart.series[0].setData(arraySAT);
+
 });
 
 $(document).on('pageshow', '#graficos', function() {
 	var chart;
+	var arrayDiasMonitorados = pacientesJson[numIdPacAtual].diasMonitorados;
+	var arrayDadosHora = pacientesJson[numIdPacAtual].diasMonitorados[arrayDiasMonitorados.length-1].dadosHoras;
+	var arrayFC = [];
+
+	for(var i=0; i<arrayDadosHora.length; i++){
+		arrayFC.push([i, parseFloat(arrayDadosHora[i].frequenciaCardiaca)]);
+		
+	}
+
+
 	$(document).ready(function() {
 		chart = new Highcharts.Chart({
 			chart : {
@@ -235,15 +281,28 @@ $(document).on('pageshow', '#graficos', function() {
 			},
 			series : [{
 				name : 'FC',
-				data : [49, 55, 57, 56, 55, 58, 60, 59, 58, 60, 65, 68, 70, 73, 71, 68, 65, 63, 60, 57, 56, 55, 52, 55]
+				data : []
 			}]
 		});
 	});
+
+
+			chart.series[0].setData(arrayFC);
 
 });
 
 $(document).on('pageshow', '#graficos', function() {
 	var chart;
+	var arrayDiasMonitorados = pacientesJson[numIdPacAtual].diasMonitorados;
+	var arrayDadosHora = pacientesJson[numIdPacAtual].diasMonitorados[arrayDiasMonitorados.length-1].dadosHoras;
+	var arrayTC = [];
+
+	for(var i=0; i<arrayDadosHora.length; i++){
+		arrayTC.push([i, parseFloat(arrayDadosHora[i].temperaturaCorporea)]);
+		
+	}
+
+
 	$(document).ready(function() {
 		chart = new Highcharts.Chart({
 			chart : {
@@ -307,85 +366,28 @@ $(document).on('pageshow', '#graficos', function() {
 			},
 			series : [{
 				name : 'TC',
-				data : [36.1, 36.1, 36.3, 36.2, 36.1, 36, 35.9, 36, 36.1, 36.5, 36.7, 36.8, 36.8, 36.8, 37, 37.2, 37.3, 37.5, 36.9, 36.5, 36.4, 36.4, 36.5, 36.6]
+				data : []
 			}]
 		});
 	});
 
-});
-
-$(document).on('pageshow', '#graficos', function() {
-	var chart;
-	$(document).ready(function() {
-		chart = new Highcharts.Chart({
-			chart : {
-				renderTo : 'temperaturaCorporea',
-
-			},
-
-			exporting : {
-				enabled : false
-			},
-			credits : {
-				enabled : false
-			},
-
-			title : {
-				text : null,
-			},
-
-			xAxis : {
-
-				categories : ['07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '00', '01', '02', '03', '04', '05', '06']
-			},
-			yAxis : {
-				title : {
-					text : null
-				},
-				plotLines : [{
-					value : 0,
-					width : 1,
-					color : '#808080'
-				}]
-			},
-			tooltip : {
-
-				formatter : function() {
-					var s = '<b>' + this.x + 'hrs</b>';
-
-					$.each(this.points, function() {
-						s += '<br/>' + this.series.name + ': ' + this.y + ' °C';
-					});
-
-					return s;
-				},
-
-				shared : true
-
-			},
-			legend : {
-				labelFormatter : function() {
-					if (this.index == 0) {
-						return 'TC';
-					}
-				},
-
-				layout : 'vertical',
-				align : 'center',
-				verticalAlign : 'bottom',
-				margin : -2
-			},
-			series : [{
-				name : 'TC',
-				data : [36.1, 36.1, 36.3, 36.2, 36.1, 36, 35.9, 36, 36.1, 36.5, 36.7, 36.8, 36.8, 36.8, 37, 37.2, 37.3, 37.5, 36.9, 36.5, 36.4, 36.4, 36.5, 36.6]
-			}]
-		});
-	});
+			chart.series[0].setData(arrayTC);
 
 });
 
+
+
 $(document).on('pageshow', '#graficos', function() {
 	var chart;
+	var arrayDiasMonitorados = pacientesJson[numIdPacAtual].diasMonitorados;
+	var arrayDadosHora = pacientesJson[numIdPacAtual].diasMonitorados[arrayDiasMonitorados.length-1].dadosHoras;
+	var arrayFR = [];
+
+	for(var i=0; i<arrayDadosHora.length; i++){
+		arrayFR.push([i, parseFloat(arrayDadosHora[i].frequenciaRespiratoria)]);
+		
+	}
+
 	$(document).ready(function() {
 		chart = new Highcharts.Chart({
 			chart : {
@@ -449,16 +451,19 @@ $(document).on('pageshow', '#graficos', function() {
 			},
 			series : [{
 				name : 'FR',
-				data : [15, 13, 12, 13, 13, 14, 14, 13, 12, 15, 16, 15, 13, 17, 18, 12, 13, 15, 17, 18, 15, 16, 13, 12]
+				data : []
 			}]
 		});
 	});
+
+
+			chart.series[0].setData(arrayFR);
 
 });
 
 $(function() {
 
-	$('#monFreqCard').click(function() {
+	$('#monitorFC').on('click', 'a', function() {
 
 		var botaoFC = document.getElementById("btFreqCar");
 		botaoFC.classList.add("ui-btn-active");
@@ -482,8 +487,8 @@ $(function() {
 		$('#frequenciaCardiaca').css('display', 'flex');
 
 	});
-
 });
+
 
 $(function() {
 
@@ -516,7 +521,7 @@ $(function() {
 
 $(function() {
 
-	$('#monFreqResp').click(function() {
+	$('#monitorFR').on('click', 'a', function() {
 
 		var botaoFR = document.getElementById("btFreqResp");
 		botaoFR.classList.add("ui-btn-active");
@@ -574,7 +579,7 @@ $(function() {
 
 $(function() {
 
-	$('#monTempCorp').click(function() {
+	$('#monitorTC').on('click', 'a', function() {
 
 		var botaoTC = document.getElementById("btTempCorp");
 		botaoTC.classList.add("ui-btn-active");
@@ -632,7 +637,7 @@ $(function() {
 
 $(function() {
 
-	$('#monSatOxig').click(function() {
+	$('#monitorSat').on('click', 'a', function() {
 
 		var botaoSO = document.getElementById("btSatOxig");
 		botaoSO.classList.add("ui-btn-active");
@@ -690,7 +695,7 @@ $(function() {
 
 $(function() {
 
-	$('#monPressaoArt').click(function() {
+	$('#monitorPA').on('click', 'a', function() {
 
 		var botaoPA = document.getElementById("btPressaoArt");
 		botaoPA.classList.add("ui-btn-active");
@@ -835,7 +840,7 @@ $(function() {
 		$("#prontuarioMonitor").html('<img src="jquerymobile-files/images/icons-png/tag-white.png"/> N.P. '+numProntuario+'');
 		$("#descricaoMonitor").html('<img src="jquerymobile-files/images/icons-png/edit-white.png"/> '+descricao+'');
 
-		$("#monitorFC").html('<a id="monFreqCard" href="#graficos" class="" ><img src="img/heart.png"/></a><big><big><big>'+fc+'</big></big></big> bpm');
+		$("#monitorFC").html('<a id="monFreqCard" href="#graficos"><img src="img/heart.png"/></a><big><big><big>'+fc+'</big></big></big> bpm');
 		$("#monitorFR").html('<a id="monFreqResp" href="#graficos"><img src="img/lung.png"/></a><big><big><big>'+fr+'</big></big></big> mpm');
 		$("#monitorTC").html('<a id="monTempCorp" href="#graficos"><img src="img/thermometer.png"/></a><big><big><big>'+tc+'</big></big></big> ºC');
 		$("#monitorSat").html('<a id="monSatOxig" href="#graficos"><img src="img/blood.png"/></a><big><big><big>'+sato2+'</big></big></big> %');
